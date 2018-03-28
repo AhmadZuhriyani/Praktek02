1b320e897793847bcebc61970ba7cb6c0dd4b435
AhmadZuhriyani <izuhryan@gmail.com>
2018-03-28 16:17:30 +0800

    Mengganti Atribut (JUMLAH -> STOK) & INTANCE OBJECT

--- src/praktek02/Laptop.java
+++ src/praktek02/Laptop.java
@@ -4,14 +4,14 @@ package praktek02;
 public class Laptop {
     String  Merek;
     Integer Harga;
-    Integer Jumlah;
+    Integer Stok;
     
      
     void Penjualan(){
         System.out.println("======================");
         System.out.println("Merek   :"  +Merek);
         System.out.println("Harga   :"  +Harga);
-        System.out.println("Jumlah  :"  +Jumlah);
+        System.out.println("Stok    :"  +Stok);
         System.out.println("======================");
     }
 }
--- src/praktek02/PenjualanAksi.java
+++ src/praktek02/PenjualanAksi.java
@@ -3,6 +3,20 @@ package praktek02;
 
 public class PenjualanAksi {
     public static void main(String[] args) {
+        Laptop R1 = new Laptop();
+        Laptop R2 = new Laptop();
         
+        
+        System.out.println("HARGA LAPTOP ASUS");
+        R1.Merek    = "ASUS";
+        R1.Harga    = 10000000;
+        R1.Stok     = 10;
+            R1.Penjualan();
+            
+        System.out.println("HARGA LAPTOP ACER");
+        R2.Merek    = "ACER";
+        R2.Harga    = 9000000;
+        R2.Stok     = 4;
+            R2.Penjualan();
     }
 }
