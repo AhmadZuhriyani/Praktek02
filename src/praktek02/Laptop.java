721423e88b8ba1be0fd385c8e9147b16d193685f
AhmadZuhriyani <izuhryan@gmail.com>
2018-03-28 15:52:28 +0800

    Menambahkan Atribut & Behavior

--- src/praktek02/Laptop.java
+++ src/praktek02/Laptop.java
@@ -2,8 +2,16 @@
 package praktek02;
 
 public class Laptop {
+    String  Merek;
     Integer Harga;
     Integer Jumlah;
     
      
+    void Penjualan(){
+        System.out.println("======================");
+        System.out.println("Merek   :"  +Merek);
+        System.out.println("Harga   :"  +Harga);
+        System.out.println("Jumlah  :"  +Jumlah);
+        System.out.println("======================");
+    }
 }
